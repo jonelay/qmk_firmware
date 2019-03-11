77ff7c100bfa594951d9490e8d2aceca1d61a670
Jone Lay <jlay.dev@pm.me>
2019-03-10 21:37:35 -0600

    update thumb keys

--- keyboards/dactyl-manuform/keymaps/6x6-jone/keymap.c
+++ keyboards/dactyl-manuform/keymaps/6x6-jone/keymap.c
@@ -73,27 +73,25 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = KEYMAP_6x6(
   // left hand
-//   KC_F1,           KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,
-   RESET,  _______,  _______,  _______,  _______,  _______,
+   KC_F12,  KC_F1,   KC_F2,   KC_F3,    KC_F4,  KC_F5,
    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
    ALT_TIL, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,
    KC_RCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,
    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,
                              KC_BSPC, KC_DEL,
                                  KC_ENTER, KC_SPC,
-                                   TT(_NAVIGATION), TT(_NUMBERS),
-                                   KC_LALT, KC_LGUI,
+                                   LT(_DN_L), LT(_UP_L),
+                                   LT(_SMSL), LT(_SM_L),
   // right hand
-//    KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    _______,  _______,  _______,  _______,  _______,  RESET,
+    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, ALT_DEL,
     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
                        KC_BSPC, KC_DEL,
                 KC_SPC, KC_ENTER,
-        TT(_NUMBERS), TT(_NAVIGATION),
-        KC_RGUI, KC_RALT),
+        LT(_UP_R), LT(_DN_R),
+        LT(_SM_R), LT(_SMSR),
 [_UP_L] = KEYMAP_6x6(
   // left hand
     _______,   _______, _______, _______, _______, _______,
